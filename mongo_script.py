e012481a8068a0101a915ac62e7b32645143c294
luco17 <lucodonoghue@gmail.com>
2019-04-01 11:15:18 +0100

    'set operations and learning to use Distinct'

--- mongo_script.py
+++ mongo_script.py
@@ -54,3 +54,37 @@ db.laureates.count_documents(criteria)
 #not equal operator
 criteria = {'bornCountry': {'$ne': 'USA'}, 'diedCountry': 'USA'}
 db.laureates.count_documents(criteria)
+
+#Dot notation
+##This allows you to access subdocuments within a document. In this example, accessing the prizes sub-doc
+criteria = {'bornCountry': 'Austria', 'prizes.affiliations.country': {'$ne': 'Austria'}}
+db.laureates.count_documents(criteria)
+
+##Checking that there are laureates with no 'born' field
+criteria = {'born': {'$exists': False}}
+assert db.laureates.count_documents(criteria) == 0
+
+##Finding laureates with more than two prizes
+criteria = {'prizes.2': {'$exists': True}}
+db.laureates.find_one(criteria)
+
+#Using the DICTINCT method
+##Checking for equality amongst category fields in laureates and prizes
+assert set(db.prizes.distinct('category')) == set(db.laureates.distinct('prizes.category'))
+
+##Countries that existed at the time of birth, but not at death
+countries = set(db.laureates.distinct('diedCountry')) - set(db.laureates.distinct('bornCountry'))
+
+##Counts of born countries, died countries and institution countries
+len(db.laureates.distinct('bornCountry'))
+len(db.laureates.distinct('diedCountry'))
+len(db.laureates.distinct('prizes.affiliations.country'))
+
+#Pre-filtering
+##Laureates born in the USA but affiliated with another country
+db.laureates.distinct('prizes.affiliations.country', {'bornCountry': 'USA'})
+
+##Checking that multi-laureates don't exist in literature
+criteria = {'laureates.2' : {'$exists': True}}
+tp_cats = set(db.prizes.distinct('category', criteria))
+assert set(db.prizes.distinct('category')) - tp_cats == {'literature'}
