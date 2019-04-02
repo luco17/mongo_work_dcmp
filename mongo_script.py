9c27375cfac2d1fd46d0fd84337c68c6ab1782ba
luco17 <lucodonoghue@gmail.com>
2019-04-02 21:17:35 +0100

    elemMatch, Regex, Projection

--- mongo_script.py
+++ mongo_script.py
@@ -1,5 +1,6 @@
 import os, requests
 from pymongo import MongoClient
+from bson.regex import Regex
 
 #Connecting to MongoClient
 client = MongoClient()
@@ -15,7 +16,6 @@ for collection_name in ['prizes', 'laureates']:
 
 assert client.nobel == db
 assert db.prizes == db['prizes']
-
 
 #Looking at all DBs within the MongoClient
 client.list_database_names()
@@ -88,3 +88,60 @@ db.laureates.distinct('prizes.affiliations.country', {'bornCountry': 'USA'})
 criteria = {'laureates.2' : {'$exists': True}}
 tp_cats = set(db.prizes.distinct('category', criteria))
 assert set(db.prizes.distinct('category')) - tp_cats == {'literature'}
+
+#Working with elem-matching to perform multiple filters
+##Finding unshared prizes in physics since 1945, note the use of the $gte, $gt operators
+criteria = {'prizes': {'$elemMatch': {
+    'category': 'physics',
+    'share': '1',
+    'year': {'$gte': '1945'}
+}}}
+
+db.laureates.count_documents(criteria)
+
+##Note the use of the $nin operator
+unshared = {
+    "prizes": {'$elemMatch': {
+        'category': {'$nin': ["physics", "chemistry", "medicine"]},
+        "share": "1",
+        "year": {'$gte': "1945"},
+    }}}
+
+shared = {
+    "prizes": {'$elemMatch': {
+        'category': {'$nin': ["physics", "chemistry", "medicine"]},
+        "share": {'$gt': "1"},
+        "year": {'$gte': "1945"},
+    }}}
+
+##Ratio of unshared to shared laureates in disciplines other than phys, chem, med
+db.laureates.count_documents(unshared) / db.laureates.count_documents(shared)
+
+#Filtering with Regex
+##Finding laureates whose first name begins with 'G'and surname begins with 'S'
+db.laureates.count_documents({'firstname': Regex('^G'), 'surname': Regex('^S')})
+
+##Germany filtering
+###All mentions of Germany
+criteria = {'bornCountry': Regex('Germany')}
+db.laureates.distinct('bornCountry', criteria)
+
+###Countries that were Germany but have reverted
+criteria = {'bornCountry': Regex('^Germany' + ' \(' + 'now')}
+db.laureates.distinct('bornCountry', criteria)
+
+###Countries that were something else but are now Germany
+criteria = {'bornCountry': Regex('now ' + 'Germany\)' + '$' )}
+db.laureates.distinct('bornCountry', criteria)
+
+##Printing the names of laureates who invented the transistor, using a list comprehension
+criteria = {'prizes.motivation': Regex('transistor')}
+first, last = 'firstname', 'surname'
+[(laureate[first], laureate[last]) for laureate in db.laureates.find(criteria)]
+
+#Projection (similar to selecting columns in a table)
+##Returning a collection of only three fields, it returns a pymongo cursor so must be saved to list or set
+list(db.laureates.find({}, {'firstname': 1, 'surname': 1, 'prizes.share': 1, '_id': 0}))[:3]
+
+##below syntax returns the same, but can't deselect the _id field
+list(db.laureates.find({}, ['firstname', 'surname', 'prizes.share']))[:3]
